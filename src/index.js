c1bd33801bf7382e949af2541b3b4c69767971c7
mla387 <mla387@sfu.ca>
2024-01-20 15:55:24 -0800

    firebase setup?

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import {initializeApp} from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCrbs-FJBu_oBISTdx_8DnM1JIz2rec1Kc",
+    authDomain: "nwhacks2024-74042.firebaseapp.com",
+    projectId: "nwhacks2024-74042",
+    storageBucket: "nwhacks2024-74042.appspot.com",
+    messagingSenderId: "147348273717",
+    appId: "1:147348273717:web:cbea9631cb3337667a0f7c",
+    measurementId: "G-1NCZD9R798"
+  };
+
+initializeApp(firebaseConfig)
